2254f892c1710bc07d114f4d4ed6f24d94298dc2
Shuxun.Li <shuxun.li@zxinsight>
2015-08-12 16:43:58 +0800

    Initial Commit

---
+++ Bluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bluetooth
+//
+//  Created by yiqiwan on 15/8/12.
+//  Copyright (c) 2015年 yiqiwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
